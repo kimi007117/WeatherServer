cd461ea3079700b3a3454f6755223d8963d09c28
kimi007117 <lj5077929@163.com>
2016-12-07 15:19:05 +0800

    【update】测试jackson

--- weather/src/main/java/com/weather/server/controller/TestController.java
+++ weather/src/main/java/com/weather/server/controller/TestController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by kimi-li on 2016/12/6.
  * 测试
@@ -13,7 +16,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
     @ResponseBody
     @RequestMapping(value = "/api/test", method = RequestMethod.GET)
-    public String getHello() {
-        return "WeatherServer Api Test";
+    public Map getHello() {
+        Map<String,Object> map = new HashMap<>();
+        map.put("id",100100);
+        map.put("city","北京");
+        return map;
     }
 }
